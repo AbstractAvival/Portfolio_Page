95bc92a1dbe824713f7afdbfad94fc8191a830a9
Avival <chrisFJ.avival@gmail.com>
2026-01-26 21:00:17 -0600

    Added background color to portfolio body.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,21 @@
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter'
+
+// Styles
+import styles from '../styles/portfolio.module.css'
+
 export const metadata = {
-  title: "Portfolio Page",
-  description: "Frontend for a developer portfolio page."
+  title: `${ process.env.NEXT_PUBLIC_TAB_TITLE }`,
+  description: `${ process.env.NEXT_PUBLIC_TAB_DESCRIPTION }`
 }
 
 export default function RootLayout( { children } ) {
   return (
     <html lang="en">
-      <body>{ children }</body>
+      <body className={ styles.body } >
+        <AppRouterCacheProvider>
+          { children }
+        </AppRouterCacheProvider>
+      </body>
     </html>
   )
 }
