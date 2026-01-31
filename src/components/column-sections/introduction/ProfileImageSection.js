3caa3ea55832c4598852f87d711c8ce363be19f4
Avival <chrisFJ.avival@gmail.com>
2026-01-31 15:04:57 -0600

    Quick component folder restructuring. Renamed sections to column-sections, created section-components folder.

--- src/components/column-sections/introduction/ProfileImageSection.js
+++ src/components/column-sections/introduction/ProfileImageSection.js
@@ -1,10 +1,8 @@
-import { useTranslations } from 'next-intl'
-
 // ** MUI components
 import Box from '@mui/material/Box'
 import Typography from "@mui/material/Typography"
 
-const ProfileImageSection = () => {
+const ProfileImageSection = ( { profileImagePosition } ) => {
     const translations = useTranslations()
     return (
         <Box>
