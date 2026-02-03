b8146e7d34a055d2c3c5d43c11521b1aa3aa2b14
Avival <chrisFJ.avival@gmail.com>
2026-02-02 21:58:04 -0600

    Skeleton implementation of SkillDisplaySection.

---
+++ src/components/column-sections/skills/SkillDisplaySection.js
@@ -0,0 +1,27 @@
+// ** MUI components
+import Grid from '@mui/material/Grid'
+
+const SkillDisplaySection = () => {
+    return (
+        <Grid container
+            sx={ {
+                margin: 12
+            } }
+        >
+            <Grid size={ 6 }>
+
+            </Grid>
+            <Grid size={ 6 }>
+
+            </Grid>
+            <Grid size={ 6 }>
+
+            </Grid>
+            <Grid size={ 6 }>
+
+            </Grid>
+        </Grid>
+    )
+}
+
+export default SkillDisplaySection
